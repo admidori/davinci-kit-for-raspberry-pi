a9376bb67a1e98b9a229c8a8d02d4e485c915318
Aoi Kondo <agotadmidori@gmail.com>
2023-12-19 02:04:53 +0900

    Update server's programs
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

---
+++ server/plot.py
@@ -0,0 +1,3 @@
+def plot_xyz(dataname,data):
+    print(dataname)
+    print(data)
---
+++ server/receiver.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+import json
+
+import plot
+
+app = Flask(__name__)
+CORS(app)
+app.config["JSON_AS_ASCII"] = False
+
+@app.route("/print", methods=['POST','GET'])
+def receive_data():
+    try:
+        json_data = request.get_json()
+        data = json.loads(json_data)
+
+        print(data)
+        return json_data
+    
+    except Exception as e:
+        return str(e)
+
+@app.route("/plot", methods=['POST','GET'])
+def plot_data():
+    json_data = request.get_json()
+    data_dict = json.loads(json_data)
+
+    # Convert dict to list
+    dataname = list(data_dict.keys())
+    data = list(data_dict.values())
+    plot.plot_xyz(dataname,data)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=False)
